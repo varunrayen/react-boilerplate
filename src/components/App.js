084aff534fa3a8e20afc7c24ab5b589164881f57
varunrayen <varunrayen@gmail.com>
2018-06-09 15:23:34 -0400

    Intial Commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+                <h4>This is a boilerplate</h4>
+            </div>
+        );
+    }
+}
+
+export default App;
